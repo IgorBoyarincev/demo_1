0231168a68e9f8c74f1bd59c7af09b86b0292b01
IgorBoyarincev <boyarincev.igor@list.ru>
2022-08-30 17:27:57 +0300

    add branch longer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {45, 17, 35, 40, 51, 23, 54, 88, 4, 77};
+        long[] sales = {45, 17, 35, 40, 51, 23, 54, 88, 4, 77};
         SalesManage sm = new SalesManage(sales);
         System.out.println(sm.max());
     }
